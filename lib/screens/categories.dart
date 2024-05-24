6041e826f23b1e60589f2ed693e7a09f1907ef58
Nisitha Athukorala <athukoralanisitha@gmail.com>
2024-05-24 19:12:42 +0530

    app wide state included

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -7,12 +7,8 @@ import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen(
-      {super.key,
-      required this.onToggleFavorite,
-      required this.availableMeals});
+  const CategoriesScreen({super.key, required this.availableMeals});
 
-  final void Function(Meal meal) onToggleFavorite;
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
@@ -23,7 +19,6 @@ class CategoriesScreen extends StatelessWidget {
       builder: (ctx) => MealsScreen(
         title: category.title,
         meals: filteredMeals,
-        onToggleFavorite: onToggleFavorite,
       ),
     )); // same as Navigator.push(context,  route)
   }
